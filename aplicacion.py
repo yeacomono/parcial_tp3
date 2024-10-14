2a534e33e8949eb8b7cd9fb680fbdca7e210caf0
ChapiTec <chapitec@Mac.mshome.net>
2024-10-14 15:43:11 -0500

    EP Damiano_Barquero Parte02

---
+++ aplicacion.py
@@ -0,0 +1,95 @@
+
+def login():
+    counter = 0
+    isError = False
+    while(True):
+        username = input('Ingrese el nombre de usuario: ')
+        contraseña = input('Ingrese su contraseña: ')
+        if(counter==1):
+            isError = True
+            break
+        login = open("login.txt","rt", encoding='utf8')
+        datos_login = login.read()
+        print(datos_login)
+
+        clave = open("clave.txt","rt", encoding='utf8')
+        datos_clave = clave.read()
+        print(datos_clave)
+        
+        if(username == datos_login and contraseña == datos_clave):
+            print("Logeado correctamente")
+            break
+        else:
+            print("Contraseña erronea ingrese nuevamente")
+        
+        counter = counter+1
+    
+    if(isError):
+        print('Supero el numero de intentos')
+        quit()
+
+
+
+
+
+
+def mostrar_menu(opciones):
+    print('Seleccione una opción:')
+    for clave in sorted(opciones):
+        print(f' {clave}) {opciones[clave][0]}')
+
+
+def leer_opcion(opciones):
+    while (a := input('Opción: ')) not in opciones:
+        print('Opción incorrecta, vuelva a intentarlo.')
+    return a
+
+
+def ejecutar_opcion(opcion, opciones):
+    opciones[opcion][1]()
+    
+def generar_menu(opciones, opcion_salida):
+    opcion = None
+    while opcion != opcion_salida:
+        mostrar_menu(opciones)
+        opcion = leer_opcion(opciones)
+        ejecutar_opcion(opcion, opciones)
+        print()
+
+
+def listar_personas():
+    pass
+
+def agregar_personas():
+    pass
+
+
+def exit():
+    print('Saliendo')
+    quit()
+    pass
+
+
+
+
+
+
+
+
+
+
+def main():
+    
+    login()
+    
+    opciones = {
+    '1': ('Listar Personas', listar_personas),
+    '2': ('Agregar Personas', agregar_personas),
+    '3': ('Salir', exit)
+    }
+    
+    generar_menu(opciones,'4')
+
+
+
+main()
